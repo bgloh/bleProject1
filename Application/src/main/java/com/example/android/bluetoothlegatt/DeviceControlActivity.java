2cb5e65c891ebd409c1c94779df9f59f38839daf
bgloh <bgloh@naver.com>
2017-07-05 22:49:09 +0900

    revision history added

--- Application/src/main/java/com/example/android/bluetoothlegatt/DeviceControlActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceControlActivity.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *  modified 7/5/2017
  */
 
 package com.example.android.bluetoothlegatt;
