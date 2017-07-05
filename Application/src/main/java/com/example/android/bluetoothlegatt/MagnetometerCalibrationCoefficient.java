5b09545b80aed4fd208e5201383593d76948d2f9
bgloh <bgloh@naver.com>
2017-07-05 22:38:46 +0900

    first ble commit

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/MagnetometerCalibrationCoefficient.java
@@ -0,0 +1,12 @@
+package com.example.android.bluetoothlegatt;
+
+/**
+ * Created by bgloh on 2017-06-28.
+ */
+
+import  com.example.android.bluetoothlegatt.Point3D;
+
+public enum MagnetometerCalibrationCoefficient {
+    INSTANCE;
+    Point3D val = new Point3D(0.0,0.0,0.0);
+}
